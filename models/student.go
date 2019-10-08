0c5718813ba49d5217b733a2d0bd048e0bd127a4
ehmad-kamal-99 <ehmad.kamal.99@gmail.com>
2019-10-08 13:10:22 +0500

    add mongodb implementation

--- models/student.go
+++ models/student.go
@@ -5,10 +5,11 @@ import (
 )
 
 type Student struct {
-	Name       string `json:"name" structs:"name"`
-	Age        string `json:"age" structs:"age"`
-	Department string `json:"department" structs:"department"`
-	Grade      int    `json:"grade" structs:"grade"`
+	ID         string `json:"id" structs:"id"  bson:"_id" structs:"id"`
+	Name       string `json:"name" structs:"name"  bson:"name" structs:"name"`
+	Age        string `json:"age" structs:"age" bson:"age" structs:"age"`
+	Department string `json:"department" structs:"department" bson:"department" structs:"department"`
+	Grade      int    `json:"grade" structs:"grade" bson:"grade" structs:"grade"`
 }
 
 func (s *Student) Map() map[string]interface{} {

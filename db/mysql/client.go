15a08cde70fea4d857b56c359d2c24494b4c3ba6
Talha927 <talhayounus1996@gmail.com>
2019-10-09 15:11:52 +0500

    mysql db impl

---
+++ db/mysql/client.go
@@ -0,0 +1,134 @@
+package mysql
+
+import (
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"strings"
+
+	"github.com/ahab94/ApnaSchool/db"
+	"github.com/ahab94/ApnaSchool/models"
+)
+
+const (
+	databaseName     = "apna_school"
+	studentTableName = "student"
+	teacherTableName = "teacher"
+	databasePassword = "Talha1996@gmail.com"
+	databaseUser     = "talha"
+)
+
+func init() {
+	db.Register("mysql", NewClient)
+}
+
+//The first implementation.
+type client struct {
+	db *sqlx.DB
+}
+
+func formatDSN() string {
+	cfg := mysql.NewConfig()
+	cfg.Net = "tcp"
+	cfg.Addr = fmt.Sprintf("%s:%s", "localhost", "3306")
+	cfg.DBName = databaseName
+	cfg.ParseTime = true
+	cfg.User = databaseUser
+	cfg.Passwd = databasePassword
+	return cfg.FormatDSN()
+}
+
+func NewClient(conf db.Option) (db.DataStore, error) {
+	cli, err := sqlx.Connect("mysql", formatDSN())
+	if err != nil {
+		return nil, err
+	}
+	return &client{db: cli}, nil
+}
+
+func (c *client) AddStudent(student *models.Student) (string, error) {
+	names := student.Names()
+	if _, err := c.db.NamedExec(fmt.Sprintf(`INSERT INTO %s (%s) VALUES(%s)`, studentTableName, strings.Join(student.Names(), ","), strings.Join(mkPlaceHolder(names, ":", func(name, prefix string) string {
+		return prefix + name
+	}), ",")), student); err != nil {
+		return "", err
+	}
+
+	return "", nil
+}
+
+func (c *client) GetStudent(id string) (*models.Student, error) {
+	var stu models.Student
+	if err := c.db.Get(&stu, fmt.Sprintf(`SELECT * FROM %s WHERE id = '%s'`, studentTableName, id)); err != nil {
+		return nil, err
+	}
+
+	return &stu, nil
+}
+
+func (c *client) UpdateStudent(student *models.Student) error {
+	names := student.Names()
+	if _, err := c.db.NamedExec(fmt.Sprintf(`UPDATE %s SET %s`, studentTableName, strings.Join(mkPlaceHolder(names, "=:", func(name, prefix string) string {
+		return name + prefix + name
+	}), ",")), student); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *client) DeleteStudent(id string) error {
+	if _, err := c.db.Query(fmt.Sprintf(`DELETE FROM %s WHERE id= '%s'`, studentTableName, id)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *client) AddTeacher(teacher *models.Teacher) (string, error) {
+	names := teacher.Names()
+	if _, err := c.db.NamedExec(fmt.Sprintf(`INSERT INTO %s (%s) VALUES(%s)`, teacherTableName, strings.Join(teacher.Names(), ","), strings.Join(mkPlaceHolder(names, ":", func(name, prefix string) string {
+		return prefix + name
+	}), ",")), teacher); err != nil {
+		return "", err
+	}
+
+	return "", nil
+}
+
+func (c *client) GetTeacher(id string) (*models.Teacher, error) {
+	var tch models.Teacher
+	if err := c.db.Get(&tch, fmt.Sprintf(`SELECT * FROM %s WHERE id = '%s'`, teacherTableName, id)); err != nil {
+		return nil, err
+	}
+
+	return &tch, nil
+}
+
+func (c *client) UpdateTeacher(teacher *models.Teacher) error {
+	names := teacher.Names()
+	if _, err := c.db.NamedExec(fmt.Sprintf(`UPDATE %s SET %s`, teacherTableName, strings.Join(mkPlaceHolder(names, "=:", func(name, prefix string) string {
+		return name + prefix + name
+	}), ",")), teacher); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *client) DeleteTeacher(id string) error {
+	if _, err := c.db.Query(fmt.Sprintf(`DELETE FROM %s WHERE id= '%s'`, teacherTableName, id)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func mkPlaceHolder(names []string, prefix string, formatName func(name, prefix string) string) []string {
+	ph := make([]string, len(names))
+	for i, name := range names {
+		ph[i] = formatName(name, prefix)
+	}
+
+	return ph
+}

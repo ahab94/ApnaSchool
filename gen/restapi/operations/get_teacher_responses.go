48c0c94f034fd2a8771cd0b11837dae6c2cfc68b
ehmad-kamal-99 <ehmad.kamal.99@gmail.com>
2019-10-24 15:08:29 +0500

    add api handlers for teachers

--- gen/restapi/operations/get_teacher_responses.go
+++ gen/restapi/operations/get_teacher_responses.go
@@ -9,6 +9,8 @@ import (
 	"net/http"
 
 	"github.com/go-openapi/runtime"
+
+	models "github.com/ahab94/ApnaSchool/gen/models"
 )
 
 // GetTeacherOKCode is the HTTP code returned for type GetTeacherOK
@@ -19,6 +21,11 @@ const GetTeacherOKCode int = 200
 swagger:response getTeacherOK
 */
 type GetTeacherOK struct {
+
+	/*
+	  In: Body
+	*/
+	Payload *models.Teacher `json:"body,omitempty"`
 }
 
 // NewGetTeacherOK creates GetTeacherOK with default headers values
@@ -27,12 +34,27 @@ func NewGetTeacherOK() *GetTeacherOK {
 	return &GetTeacherOK{}
 }
 
+// WithPayload adds the payload to the get teacher o k response
+func (o *GetTeacherOK) WithPayload(payload *models.Teacher) *GetTeacherOK {
+	o.Payload = payload
+	return o
+}
+
+// SetPayload sets the payload to the get teacher o k response
+func (o *GetTeacherOK) SetPayload(payload *models.Teacher) {
+	o.Payload = payload
+}
+
 // WriteResponse to the client
 func (o *GetTeacherOK) WriteResponse(rw http.ResponseWriter, producer runtime.Producer) {
 
-	rw.Header().Del(runtime.HeaderContentType) //Remove Content-Type on empty responses
-
 	rw.WriteHeader(200)
+	if o.Payload != nil {
+		payload := o.Payload
+		if err := producer.Produce(rw, payload); err != nil {
+			panic(err) // let the recovery middleware deal with this
+		}
+	}
 }
 
 // GetTeacherNotFoundCode is the HTTP code returned for type GetTeacherNotFound
